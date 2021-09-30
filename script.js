738dfc8763093ee933e8f2539959017fd9775f63
Atul chaudhary <91272081+atul526@users.noreply.github.com>
2021-09-30 16:05:39 +0530

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+function go(){
+    alert(" My father Name is Ram Prasad chaudhary, My Mother  Name is Rupa devi and I have 1 Brother and 1 Sister");
+   document.write("My self Atul chaudhary ")
+   document.write("I'm currently doing B.tech in Computer sceinece and engineering from Moden Mohan Malviya University of Technology Gorakhpur(u.p)")
+   document.body.style.backgroundColor = "aqua";
+}
